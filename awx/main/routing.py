bf43b85c0677017c215913280cfa45f71f17b3d0
Ryan Petrello <rpetrell@redhat.com>
2020-07-23 10:19:03 -0400

    DONOTMERGE: add memory allocation tracing to Daphne

--- awx/main/routing.py
+++ awx/main/routing.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+import tracemalloc
+
 import redis
 import logging
 
@@ -10,11 +13,16 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from . import consumers
 
 
+TRACE_INTERVAL = 60 * 5  # take a tracemalloc snapshot every 5 minutes
+
+
 logger = logging.getLogger('awx.main.routing')
 
 
 class AWXProtocolTypeRouter(ProtocolTypeRouter):
     def __init__(self, *args, **kwargs):
+        tracemalloc.start()
+
         try:
             r = redis.Redis.from_url(settings.BROKER_URL)
             for k in r.scan_iter('asgi:*', 500):
@@ -26,8 +34,26 @@ class AWXProtocolTypeRouter(ProtocolTypeRouter):
         super().__init__(*args, **kwargs)
 
 
+class SnapshottedEventConsumer(consumers.EventConsumer):
+
+    last_snapshot = datetime.min
+
+    async def connect(self):
+        if (datetime.now() - SnapshottedEventConsumer.last_snapshot).total_seconds() > TRACE_INTERVAL:
+            snapshot = tracemalloc.take_snapshot()
+            top_stats = snapshot.statistics('lineno')
+            top_stats = [
+                stat for stat in top_stats[:10]
+                if 'importlib._bootstrap_external' not in str(stat)
+            ]
+            for stat in top_stats[:5]:
+                logger.error('[TRACE] ' + str(stat))
+            SnapshottedEventConsumer.last_snapshot = datetime.now()
+        await super().connect()
+
+
 websocket_urlpatterns = [
-    url(r'websocket/$', consumers.EventConsumer),
+    url(r'websocket/$', SnapshottedEventConsumer),
     url(r'websocket/broadcast/$', consumers.BroadcastConsumer),
 ]
 
